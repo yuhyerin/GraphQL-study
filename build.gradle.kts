585c9c2befb676d0797ac718e457ceabc3ae3a8e
yuhyerin <hyerin77@naver.com>
2024-11-29 19:03:03 +0900

    프로젝트 세팅 및 스키마 작성

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-graphql")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework:spring-webflux")
+	testImplementation("org.springframework.graphql:spring-graphql-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// custom scalar
+	implementation("com.graphql-java:graphql-java-extended-scalars:22.0")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/example/goopang/GraphQLConfig.kt
@@ -0,0 +1,17 @@
+package com.example.goopang
+
+import graphql.scalars.ExtendedScalars
+import graphql.schema.idl.RuntimeWiring
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.graphql.execution.RuntimeWiringConfigurer
+
+@Configuration
+class GraphQLConfig {
+    @Bean
+    fun runtimeWiringConfigurer(): RuntimeWiringConfigurer {
+        return RuntimeWiringConfigurer { wiringBuilder: RuntimeWiring.Builder ->
+            wiringBuilder.scalar(ExtendedScalars.DateTime)
+        }
+    }
+}
